5bcdf9d3c96400ff2c83a088fd1f66c0988359dd
Akira Nakajima <nakajima@altonotes.co.jp>
2020-02-27 12:46:48 +0900

    サンプルの移植（ポーリング）

---
+++ app/src/main/java/kibaan/android/abstracthttp/commonspec/FXRateListAPI.kt
@@ -0,0 +1,51 @@
+package kibaan.android.abstracthttp.commonspec
+
+import abstracthttp.core.ConnectionSpec
+import abstracthttp.entity.Response
+import abstracthttp.entity.URLQuery
+import abstracthttp.enumtype.HTTPMethod
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class FXRateListAPI : ConnectionSpec<FXRateList> {
+    override val url: String
+        get() = "https://www.gaitameonline.com/rateaj/getrate"
+
+    override val httpMethod: HTTPMethod
+        get() = HTTPMethod.get
+
+    override val headers: Map<String, String>
+        get() = mapOf()
+
+    override val urlQuery: URLQuery?
+        get() = null
+
+    override val body: ByteArray?
+        get() = null
+
+    override fun validate(response: Response): Boolean = response.statusCode == 200
+
+    override fun parseResponse(response: Response): FXRateList {
+        val json = response.data.toString(Charsets.UTF_8)
+        val type = TypeToken.getParameterized(FXRateList::class.java).type
+        return Gson().fromJson(json, type)
+    }
+}
+
+data class FXRateList(val quotes: List<FXRate>) {}
+
+data class FXRate(
+    val currencyPairCode: String,
+    val open: String?,
+    val high: String?,
+    val low: String?,
+    val bid: String?,
+    val ask: String?
+) {
+    val stringValue: String
+        get() {
+            val bidText = bid ?: "--"
+            val askText = ask ?: "--"
+            return "[${currencyPairCode}]  $bidText - $askText"
+        }
+}
---
+++ app/src/main/java/kibaan/android/abstracthttp/examples/polling/PollingActivity.kt
@@ -0,0 +1,93 @@
+package kibaan.android.abstracthttp.examples.polling
+
+import abstracthttp.core.Connection
+import abstracthttp.defaultimpl.Polling
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kibaan.android.abstracthttp.ExampleItem
+import kibaan.android.abstracthttp.commonspec.FXRateList
+import kibaan.android.abstracthttp.commonspec.FXRateListAPI
+import kibaan.android.abstracthttp.examples.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class PollingViewController : FragmentActivity(), ExampleItem {
+
+    override val displayTitle: String
+        get() = "ポーリング（自動更新）"
+
+    lateinit var timeLabel: TextView
+    lateinit var recyclerView: RecyclerView
+
+    private val polling by lazy {
+        Polling(delaySeconds = 1, callback = ::request)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_polling)
+
+        timeLabel = findViewById(R.id.timeLabel)
+        timeLabel.text = null
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecyclerAdapter(this, rateList = null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        request()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        polling.stop()
+    }
+
+    private fun request() {
+        Connection(FXRateListAPI()) { response ->
+            this.update(rateList = response)
+        }.addListener(polling).start()
+    }
+
+    private fun update(rateList: FXRateList) {
+        (recyclerView.adapter as? RecyclerAdapter)?.rateList = rateList
+        recyclerView.adapter?.notifyDataSetChanged()
+        val dateFormatter = SimpleDateFormat("HH:mm:ss", Locale.US)
+        timeLabel.text = dateFormatter.format(Date())
+    }
+
+    inner class RecyclerAdapter(context: Context, var rateList: FXRateList?) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+        private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(inflater.inflate(R.layout.list_cell_getjson, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return rateList?.quotes?.size ?: 0
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val quote = rateList?.quotes?.get(position)
+            holder.textView.text = quote?.stringValue
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var textView: TextView = itemView.findViewById(R.id.textView)
+        }
+    }
+}
