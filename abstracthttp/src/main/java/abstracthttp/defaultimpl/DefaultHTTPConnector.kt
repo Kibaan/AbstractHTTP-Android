7801b874df6675234043cee15ece1ae5b6e07fb8
Akira Nakajima <nakajima@altonotes.co.jp>
2020-03-03 16:00:08 +0900

    DefaultHTTPConnectorの非同期処理をAsyncTaskからCoroutineで実現するように修正

--- abstracthttp/src/main/java/abstracthttp/defaultimpl/DefaultHTTPConnector.kt
+++ abstracthttp/src/main/java/abstracthttp/defaultimpl/DefaultHTTPConnector.kt
@@ -3,16 +3,15 @@ package abstracthttp.defaultimpl
 import abstracthttp.core.HTTPConnector
 import abstracthttp.entity.Request
 import abstracthttp.entity.Response
-import abstracthttp.enumtype.HTTPMethod
-import android.os.AsyncTask
+import android.util.Log
+import kotlinx.coroutines.*
 import java.io.IOException
 import java.io.InputStream
-import java.io.OutputStream
 import java.net.CookieHandler
 import java.net.CookieManager
 import java.net.HttpURLConnection
 import java.net.URL
-
+import kotlin.coroutines.CoroutineContext
 
 /**
  * HTTP通信の標準実装
@@ -30,6 +29,9 @@ class DefaultHTTPConnector : HTTPConnector {
     /** Cookieを有効にするか */
     var isCookieEnabled = true
 
+    /** ログ出力するか */
+    var isLogEnabled = false
+
     /** 通信中の[HTTPTask] */
     private var httpTask: HTTPTask? = null
 
@@ -37,19 +39,25 @@ class DefaultHTTPConnector : HTTPConnector {
         val config = HTTPConfig(
             timeout = (timeoutInterval * 1000.0).toInt(),
             isRedirectEnabled = isRedirectEnabled,
-            isCookieEnabled = isCookieEnabled
+            isCookieEnabled = isCookieEnabled,
+            isLogEnabled = isLogEnabled
         )
-        httpTask = HTTPTask(config, complete).execute(request) as? HTTPTask
+        httpTask = HTTPTask(config, complete)
+        httpTask?.execute(request)
     }
 
     override fun cancel() {
-        httpTask?.cancel(true)
+        httpTask?.cancel()
     }
 
-    class HTTPTask(
-        private val config: HTTPConfig,
-        private val complete: (Response?, Exception?) -> Unit
-    ) : AsyncTask<Request, Void, Response?>() {
+    class HTTPTask(private val config: HTTPConfig, private val complete: (Response?, Exception?) -> Unit) : CoroutineScope {
+
+        private val logTag = javaClass.simpleName
+
+        private val job = Job()
+
+        override val coroutineContext: CoroutineContext
+            get() = Dispatchers.Main + job
 
         private val cookieManager by lazy {
             var manager = CookieHandler.getDefault()
@@ -60,52 +68,44 @@ class DefaultHTTPConnector : HTTPConnector {
             return@lazy manager
         }
 
-        override fun doInBackground(vararg requests: Request): Response? {
-            return connect(request = requests.first())
+        fun execute(request: Request) {
+            launch(Dispatchers.IO) {
+                try {
+                    val response = connect(request = request)
+                    onComplete(response, null)
+                } catch (e: Exception) {
+                    onComplete(null, e)
+                }
+            }
         }
 
-        override fun onPostExecute(result: Response?) {
-            super.onPostExecute(result)
-            complete.invoke(result, null)
+        fun cancel() {
+            job.cancel()
+            log("cancel.")
         }
 
         private fun connect(request: Request): Response? {
             var connection: HttpURLConnection? = null
-            val outputStream: OutputStream?
 
             try {
                 // 接続
-                connection = request.url.openConnection() as HttpURLConnection
-
-                // メソッド指定
-                connection.requestMethod = request.method.stringValue
-                // 自動的でリダイレクトはしないように設定する（独自実装で制御）
-                connection.instanceFollowRedirects = false
-                // 接続タイムアウト指定
-                connection.connectTimeout = config.timeout
-                connection.readTimeout = config.timeout
-                // ヘッダー付与
-                setHeaderToConnection(connection, request.headers)
-
-                // リクエストボディの設定
-                if (request.body != null && request.method == HTTPMethod.post) {
-                    connection.doOutput = true
-                    outputStream = connection.outputStream
-                    outputStream.write(request.body)
-                } else {
-                    connection.doOutput = false
-                }
+                connection = makeURLConnection(request)
                 connection.connect()
+                log("connect.")
 
                 // Cookieの保存
                 if (config.isCookieEnabled) {
                     cookieManager.put(request.url.toURI(), connection.headerFields)
                 }
-                return afterProcess(connection, request)
-            } catch (e1: IOException) {
-                if (connection == null) {
-                    throw e1
+                // リダイレクト
+                val location = getRedirectLocation(connection)
+                if (location != null) {
+                    val body = if (connection.responseCode == 303) null else request.body
+                    return connect(request = Request(url = location, method = request.method, body = body, headers = request.headers))
                 }
+                return makeResponse(connection, connection.inputStream)
+            } catch (e1: IOException) {
+                if (connection == null) throw e1
                 try {
                     // 404などの場合Exceptionが発生してもレスポンスがあるので読み取り
                     return makeResponse(connection, connection.errorStream)
@@ -114,47 +114,70 @@ class DefaultHTTPConnector : HTTPConnector {
                 }
             } finally {
                 connection?.disconnect()
+                log("disconnect.")
             }
         }
 
-        private fun afterProcess(connection: HttpURLConnection, request: Request): Response? {
+        private fun onComplete(response: Response?, exception: Exception?) {
+            if (isActive) {
+                complete(response, exception)
+                log("complete.")
+            }
+        }
+
+        private fun makeURLConnection(request: Request): HttpURLConnection {
+            val connection = request.url.openConnection() as HttpURLConnection
+
+            // メソッド指定
+            connection.requestMethod = request.method.stringValue
+            // 自動的でリダイレクトはしないように設定する（独自実装で制御）
+            connection.instanceFollowRedirects = false
+            // 接続タイムアウト指定
+            // TODO iOS版と動きが異なるので、要検討
+            connection.connectTimeout = config.timeout
+            // ヘッダー付与
+            request.headers.forEach {
+                connection.setRequestProperty(it.key, it.value)
+            }
+            // リクエストボディの設定
+            if (request.body != null) {
+                connection.doOutput = true
+
+                // TODO 一発で書き込みが本当に出来るか？JavaDocとか確認する
+                connection.outputStream.write(request.body)
+            }
+            return connection
+        }
+
+        private fun getRedirectLocation(connection: HttpURLConnection): URL? {
             val statusCode = connection.responseCode
             val location = connection.headerFields["Location"]?.firstOrNull()
             if (config.isRedirectEnabled && statusCode.toString().startsWith("3") && location != null) {
-                val body = if (statusCode == 303) null else request.body
-                return connect(request = Request(url = URL(location), method = request.method, body = body, headers = request.headers))
+                return URL(location)
             }
-            return makeResponse(connection, connection.inputStream)
+            return null
         }
 
         private fun makeResponse(connection: HttpURLConnection, inputStream: InputStream?): Response? {
             val responseData = inputStream?.readBytes() ?: return null
-            val headers = getHeaderByConnection(connection)
-            return Response(data = responseData, statusCode = connection.responseCode, headers = headers, nativeResponse = null)
-        }
-
-        private fun setHeaderToConnection(connection: HttpURLConnection, headers: Map<String, String>) {
-            headers.keys.forEachIndexed { index, key ->
-                if (index == 0) {
-                    connection.setRequestProperty(key, headers[key])
-                } else {
-                    connection.addRequestProperty(key, headers[key])
-                }
-            }
-        }
-
-        private fun getHeaderByConnection(connection: HttpURLConnection): Map<String, String> {
             val headers = mutableMapOf<String, String>()
             connection.headerFields.filter { it.key != null }.forEach {
                 headers[it.key] = it.value.joinToString(" ")
             }
-            return headers
+            return Response(data = responseData, statusCode = connection.responseCode, headers = headers, nativeResponse = null)
+        }
+
+        private fun log(message: String) {
+            if (config.isLogEnabled) {
+                Log.d(logTag, message)
+            }
         }
     }
 
     data class HTTPConfig(
         val timeout: Int,
         val isRedirectEnabled: Boolean,
-        val isCookieEnabled: Boolean
+        val isCookieEnabled: Boolean,
+        val isLogEnabled: Boolean
     )
 }
