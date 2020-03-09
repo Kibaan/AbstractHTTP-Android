0874957892e4e523a6bc8b11f5abbd2b955fab71
Akira Nakajima <nakajima@altonotes.co.jp>
2020-03-09 14:03:34 +0900

    サンプル処理の実装中

--- app/src/main/java/kibaan/android/abstracthttp/MainActivity.kt
+++ app/src/main/java/kibaan/android/abstracthttp/MainActivity.kt
@@ -1,13 +1,34 @@
 package kibaan.android.abstracthttp
 
-import androidx.appcompat.app.AppCompatActivity
+import abstracthttp.core.Connection
+import abstracthttp.core.ConnectionHolder
+import abstracthttp.core.ConnectionHolderListener
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import kibaan.android.abstracthttp.examples.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ConnectionHolderListener {
+
+    lateinit var countIcon: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolBar = findViewById<Toolbar>(R.id.toolBar)
+        setSupportActionBar(toolBar)
+        countIcon = toolBar.findViewById(R.id.countIcon)
+
+        ConnectionHolder.shared.addListener(this)
+    }
+
+    override fun onAdded(connection: Connection<*>, count: Int) {
+        this.countIcon.text = count.toString()
+    }
+
+    override fun onRemoved(connection: Connection<*>, count: Int) {
+        this.countIcon.text = count.toString()
     }
 }
