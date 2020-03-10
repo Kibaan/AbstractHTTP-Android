aefadb8f1b0dcb46b91a87bf27cf1a47583b1c26
Akira Nakajima <nakajima@altonotes.co.jp>
2020-03-10 11:23:37 +0900

    androidパッケージのクラスを使用しないように修正

--- abstracthttp/src/main/java/abstracthttp/defaultimpl/Polling.kt
+++ abstracthttp/src/main/java/abstracthttp/defaultimpl/Polling.kt
@@ -6,14 +6,12 @@ import abstracthttp.entity.ConnectionError
 import abstracthttp.entity.Request
 import abstracthttp.entity.Response
 import abstracthttp.enumtype.ConnectionErrorType
-import android.os.Handler
 import java.util.*
 import kotlin.concurrent.schedule
 
 class Polling(val delaySeconds: Long, val callback: () -> Unit) : ConnectionListener {
 
-    var handler = Handler()
-    var runnable = Runnable { callback() }
+    var timerTask: TimerTask? = null
 
     var connection: Connection<*>? = null
 
@@ -24,15 +22,21 @@ class Polling(val delaySeconds: Long, val callback: () -> Unit) : ConnectionList
 
     override fun onEnd(connection: Connection<*>, response: Response?, responseModel: Any?, error: ConnectionError?) {
         if (error == null || error.type == ConnectionErrorType.network) {
-            handler.postDelayed(runnable, delaySeconds * 1000)
+            timerTask = Timer().schedule(delaySeconds * 1000) {
+                timerTask?.cancel()
+                timerTask = null
+                callback()
+            }
         } else if (error.type == ConnectionErrorType.canceled) {
-            handler.removeCallbacks(runnable)
+            timerTask?.cancel()
+            timerTask = null
         }
         this.connection = null
     }
 
     fun stop() {
-        handler.removeCallbacks(runnable)
+        timerTask?.cancel()
+        timerTask = null
         connection?.cancel()
         connection = null
     }
